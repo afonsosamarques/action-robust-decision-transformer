302e537bf98d554fec91616031631d9870a82a90
afonsosamarques <afonsosamarques@gmail.com>
2023-07-26 00:57:20 +0100

    Setting up experiment

---
+++ cluster-scripts/cscluster-run-scripts/run_experiment_1.sh
@@ -0,0 +1,17 @@
+#$ -S /bin/bash
+
+#$ -N ardt_simplest-halfcheetah-dataset_combo_v1
+#$ -l gpu=1
+#$ -l tmem=4G
+#$ -l h_rt=18:00:00
+
+#$ -wd /home/amarques/action-robust-decision-transformer/codebase
+
+source /share/apps/source_files/python/python-3.9.5.source
+source /share/apps/source_files/cuda/cuda-11.2.source
+export PATH=$PATH:/share/apps/git-lfs-2.11.0/bin/
+
+source /home/amarques/envs/ardt-env/bin/activate
+
+nvidia-smi
+python3 -m ardt.pipeline --config_name ardt_simplest-halfcheetah-dataset_combo_v1
